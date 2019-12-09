709a62f1635262f70de43a9eed665bbf93786b38
Maximilian-Stefan-Ernst <maximilian.stefan.ernst@gmail.com>
2019-12-09 17:17:39 +0100

    absolve first section

--- testfile.jl
+++ testfile.jl
@@ -1,5 +1,4 @@
-using Pkg
-using Random, Distributions
+using Pkg, Random, Distributions
 
 a = Vector{Int64}(1:5)
 
@@ -57,3 +56,87 @@ function binomial_ran(n, p)
 end
 
 binomial_ran(10, 0.9)
+
+
+################################################
+#Speed
+################################################
+
+methods(^)
+
+## function syntax
+
+myfun(x,y) = x+y
+
+myfun(x,y,z) = x+y*z
+
+myfun(x::Int64, y::Int64) = x*y
+
+methods(myfun)
+
+myfun(2.0,3)
+
+mat1 = rand(2,3)
+mat2 = rand(2,3)
+
+myfun(mat1, mat2)
+
+f(x::T,y::T) where{T} = 4x+10y
+
+f(x::T,y::T2,z::T2) where {T<:Number,T2} = 5x + 5y + 5z
+
+f(1,2,3)
+
+@code_llvm f(1,2,3)
+
+@code_native myfun(1,2)
+
+
+@time myfun(1,2)
+
+@time myfun(1.0, 2.0)
+
+myfun(x::Int64,y::Int64,z::Int64) = x+y+z
+
+function myfun(x,y)
+    return (x+y;x*y)
+end
+
+## types
+
+mutable struct Car
+    make
+    model
+end
+
+mycar = Car("Toyota","Rav4")
+
+mycar.model
+
+mutable struct StaffMember{T<:Number}
+    name::String
+    field::Symbol
+    age::T
+end
+
+ter = StaffMember("Terry",:football,17)
+
+# immutable
+struct Field
+    name
+    school
+end
+ds = Field(:DataScience,[:PhysicalScience;:ComputerScience])
+
+push!(ds.school,:BiologicalScience)
+
+ds
+
+#lazy iterator types
+b = 1:0.1:5
+
+@time c = collect(b)
+
+@time d = collect(b)
+
+d = c
